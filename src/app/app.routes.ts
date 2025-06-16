a4d347f273921883c9d33903d1be7cab250457a8
unknown <User@600000-96.ad.lan>
2025-06-16 13:52:00 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,24 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'register',component:RegisterComponent
-    },
-    {
-        path:'login',component:LoginComponent
-    }
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
 ];
